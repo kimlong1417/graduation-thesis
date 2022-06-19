1e71034759f6db86ceb70c3adfe4df3825d5c539
tranhaikimlong <tranhaikimlong1417@gmail.com>
2022-06-19 16:25:51 +0700

    version 19/6/2022

--- Face-Verify/views.py
+++ Face-Verify/views.py
@@ -13,6 +13,8 @@ from sklearn.metrics.pairwise import cosine_distances
 from werkzeug.utils import secure_filename
 from mtcnn.mtcnn import MTCNN
 import json
+from scipy.spatial import distance
+import math
 
 from .models import Image
 
@@ -35,7 +37,7 @@ def home():
 
 
 def create_path():
-    path = UPLOAD_FOLDER + 'upload/'
+    path = UPLOAD_FOLDER + 'upload/' + 'user_' + str(current_user.id) + "/"
     isExist = os.path.exists(path)
     if not isExist:
         os.makedirs(path)
@@ -43,7 +45,7 @@ def create_path():
 
 
 def result_path():
-    path = UPLOAD_FOLDER + 'result/'
+    path = UPLOAD_FOLDER + 'result/' + 'user_' + str(current_user.id) + "/"
     isExist = os.path.exists(path)
     if not isExist:
         os.makedirs(path)
@@ -65,18 +67,22 @@ def upload_image():
                 filename = secure_filename(file.filename)
                 path = os.path.join(create_path(), filename)
                 file.save(path)
-                # check user image where current_user = current_user.id, where img=path
-                user_image = Image.query.filter_by(user_id=current_user.id, img=path).first()
-                if user_image:
-                    flash('This image already exists.', category='error')
+                if check_faceNull(path,filename):
+                    # check user image where current_user = current_user.id, where img=path
+                    user_image = Image.query.filter_by(user_id=current_user.id, img=path).first()
+                    if user_image:
+                        flash('This image already exists.', category='error')
+                    else:
+                        pic = Image(img=path, user_id=current_user.id)
+                        db.session.add(pic)
+                        db.session.commit()
+                        flash('Image successfully uploaded', category='success')
                 else:
-                    pic = Image(img=path, user_id=current_user.id)
-                    db.session.add(pic)
-                    db.session.commit()
-                    flash('Image successfully uploaded', category='success')
+                    os.remove(path)
             else:
                 flash('Allowed image types are - png, jpg, jpeg, gif', category='error')
                 return redirect(request.url)
+
     return render_template("upload.html", user=current_user)
 
 
@@ -91,9 +97,9 @@ def verify_image():
             filename = secure_filename(imageA.filename)
             imageA_path = os.path.join(create_path(), filename)
             imageA.save(imageA_path)
-            imageA_pixel = extract_face(imageA_path)
+            imageA_pixel, imageA_align = extract_face(imageA_path)
             newTrain_ImageA = list()
-            imageA_embedding = get_embedding(MODEL, imageA_pixel)
+            imageA_embedding = get_embedding(MODEL, imageA_align)
             newTrain_ImageA.append(imageA_embedding)
             newTrain_ImageA = np.asarray(newTrain_ImageA)
             flash('The first image successfully uploaded', category='success')
@@ -120,30 +126,126 @@ def verify_image():
         point = 0
         for im in Img:
             try:
-                print(im.img)
-                imageB_pixel = extract_face(im.img)
+                # print(im.img)
+                imageB_pixel, imageB_align = extract_face(im.img)
                 newTrain_ImageB = list()
-                imageB_embedding = get_embedding(MODEL, imageB_pixel)
+                imageB_embedding = get_embedding(MODEL, imageB_align)
                 newTrain_ImageB.append(imageB_embedding)
                 newTrain_ImageB = np.asarray(newTrain_ImageB)
-                cosine_distances_score = compare_image(imageA_pixel,imageB_pixel,newTrain_ImageA,newTrain_ImageB,current_user.first_name)
-                print(cosine_distances_score)
+                cosine_distances_score = compare_images(imageA_align,imageB_align,newTrain_ImageA,newTrain_ImageB)
+                # print(cosine_distances_score)
                 point += 1
                 total += cosine_distances_score
             except:
                 return redirect(request.url)
 
         mean = total/point
-        # compare_image(imageA_pixel, imageB_pixel, newTrain_ImageA, newTrain_ImageB, current_user.first_name)
+
+        title = str(current_user.first_name)
+        fig = plt.figure(title,figsize=(5, 3), facecolor="#F3F1F5")
+        if (mean > 0.5):
+            plt.suptitle("TRUE: the result is " + title)
+        else:
+            plt.suptitle("FALSE: the result is not " + title)
+
+        # plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+        # show first image
+        ax1 = fig.add_subplot(1, 3, 1)
+        ax1.title.set_text("Base image")
+        img_base = plt.imread(imageA_path)
+        plt.imshow(img_base, cmap=plt.cm.gray)
+        plt.axis("off")
+        # show the second image
+        ax2 = fig.add_subplot(1, 3, 2)
+        ax2.title.set_text("Detected face")
+        plt.imshow(imageA_pixel, cmap=plt.cm.gray)
+        plt.axis("off")
+        # show the third image
+        ax3 = fig.add_subplot(1, 3, 3)
+        ax3.title.set_text("Aligned image")
+        plt.xlabel(float(mean))
+        plt.imshow(imageA_align, cmap=plt.cm.gray)
+        plt.axis("off")
+        # show the images
+        plt.savefig(result_path() + title + '.jpg')
+        plt.close()
 
         pic = current_user.first_name + '.jpg'
-        img1 = Image1.open(os.path.join(create_path(), pic))
+        img1 = Image1.open(os.path.join(result_path(), pic))
         data = io.BytesIO()
         img1.save(data, "JPEG")
 
         encode_image = base64.b64encode(data.getvalue())
-        return render_template("verify.html", pic = encode_image.decode("UTF-8"), user=current_user, mean = mean)
+        return render_template("verify.html", pic = encode_image.decode("UTF-8"), user=current_user, mean = float(mean))
     return render_template("verify.html", user=current_user)
+
+
+def check_faceNull(path,filename):
+    # load image from file
+    image = Image1.open(path)
+    # convert to RGB, if needed
+    image = image.convert('RGB')
+    # convert to array
+    pixels = asarray(image)
+    # create the detector, using default weights
+    detector = MTCNN()
+    # detect faces in the image
+    results = detector.detect_faces(pixels)
+    if len(results) > 1:
+        flash('There are more than one face in this image - ' + str(filename) + '!!!', category='error')
+        return False
+    elif len(results) <= 0:
+        flash('This image ' + str(filename) + ' does not have any faces', category='error')
+        return False
+    else:
+        return True
+
+
+def alignment_procedure(img, left_eye, right_eye):
+    # this function aligns given face in img based on left and right eye coordinates
+
+    left_eye_x, left_eye_y = left_eye
+    right_eye_x, right_eye_y = right_eye
+
+    # -----------------------
+    # find rotation direction
+
+    if left_eye_y >= right_eye_y:
+        point_3rd = (right_eye_x, left_eye_y)
+        direction = -1  # rotate same direction to clock
+    else:
+        point_3rd = (left_eye_x, right_eye_y)
+        direction = 1  # rotate inverse direction of clock
+
+    # -----------------------
+    # find length of triangle edges
+
+    a = distance.euclidean(np.array(left_eye), np.array(point_3rd))
+    b = distance.euclidean(np.array(right_eye), np.array(point_3rd))
+    c = distance.euclidean(np.array(right_eye), np.array(left_eye))
+
+    # -----------------------
+
+    # apply cosine rule
+
+    if b != 0 and c != 0:  # this multiplication causes division by zero in cos_a calculation
+
+        cos_a = (b * b + c * c - a * a) / (2 * b * c)
+        angle = np.arccos(cos_a)  # angle in radian
+        angle = (angle * 180) / math.pi  # radian to degree
+
+        # -----------------------
+        # rotate base image
+
+        if direction == -1:
+            angle = 90 - angle
+
+        img = Image1.fromarray(img)
+        img = np.array(img.rotate(direction * angle))
+
+    # -----------------------
+
+    return img  # return img anyway
 
 
 def extract_face(Image_File, required_size=(160, 160)):
@@ -157,6 +259,13 @@ def extract_face(Image_File, required_size=(160, 160)):
     detector = MTCNN()
     # detect faces in the image
     result = detector.detect_faces(pixels)
+
+    # -----------------
+    keypoints = result[0]["keypoints"]
+    left_eye = keypoints["left_eye"]
+    right_eye = keypoints["right_eye"]
+    # ------------
+
     # extract the bouding box from the first face
     x1, y1, width, height = result[0]['box']
     # bug fix
@@ -164,11 +273,14 @@ def extract_face(Image_File, required_size=(160, 160)):
     x2, y2 = x1 + width, y1 + height
     # extract the face
     face = pixels[y1:y2, x1:x2]
+
+    alignment_img = alignment_procedure(face, left_eye, right_eye)
+
     # resize pixels to the model size
-    image = Image1.fromarray(face)
+    image = Image1.fromarray(alignment_img)
     image = image.resize(required_size)
     face_array = asarray(image)
-    return face_array
+    return face, face_array
 
 
 def get_embedding(model, face_pixels):
@@ -196,29 +308,40 @@ def mse(imageA, imageB):
     return err
 
 
-def compare_image(imgA, imgB, imgA_pixel, imgB_pixel, title):
+# def compare_image(imgA, imgB, imgA_pixel, imgB_pixel, title):
+#     # compute the mean squared error and structural similarity
+#     # index for the images
+#     m = mse(imgA_pixel, imgB_pixel)
+#     spicy = 1 - spatial.distance.cosine(imgA_pixel, imgB_pixel)
+#     imgB_pixel_T = imgB_pixel.T
+#     nump = imgA_pixel.dot(imgB_pixel_T) / (np.linalg.norm(imgA_pixel, axis=1) * np.linalg.norm(imgB_pixel_T))
+#     skl = 1 - cosine_distances(imgA_pixel, imgB_pixel)
+#     # setup the figure
+#     fig = plt.figure(title)
+#     plt.suptitle("MSE: %.5f, Spicy: %.5f, Numpy: %.5f, Sklearn: %.5f" % (m, spicy, nump, skl))
+#     # plt.suptitle("Sklearn: %.5f" % skl)
+#     # show first image
+#     fig.add_subplot(1, 2, 1)
+#     plt.imshow(imgA, cmap=plt.cm.gray)
+#     plt.axis("off")
+#     # show the second image
+#     fig.add_subplot(1, 2, 2)
+#     plt.imshow(imgB, cmap=plt.cm.gray)
+#     plt.axis("off")
+#     # show the images
+#     plt.savefig(create_path() + title + '.jpg')
+#     plt.close()
+#     return skl
+
+def compare_images(path1, path2, imageA, imageB):
     # compute the mean squared error and structural similarity
     # index for the images
-    m = mse(imgA_pixel, imgB_pixel)
-    spicy = 1 - spatial.distance.cosine(imgA_pixel, imgB_pixel)
-    imgB_pixel_T = imgB_pixel.T
-    nump = imgA_pixel.dot(imgB_pixel_T) / (np.linalg.norm(imgA_pixel, axis=1) * np.linalg.norm(imgB_pixel_T))
-    skl = 1 - cosine_distances(imgA_pixel, imgB_pixel)
-    # setup the figure
-    fig = plt.figure(title)
-    plt.suptitle("MSE: %.5f, Spicy: %.5f, Numpy: %.5f, Sklearn: %.5f" % (m, spicy, nump, skl))
-    # plt.suptitle("Sklearn: %.5f" % skl)
-    # show first image
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(imgA, cmap=plt.cm.gray)
-    plt.axis("off")
-    # show the second image
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(imgB, cmap=plt.cm.gray)
-    plt.axis("off")
-    # show the images
-    plt.savefig(create_path() + title + '.jpg')
-    plt.close()
+    m = mse(imageA, imageB)
+    spicy = 1 - spatial.distance.cosine(imageA, imageB)
+    imgB_T = imageB.T
+    nump = imageA.dot(imgB_T)/ (np.linalg.norm(imageA, axis=1) * np.linalg.norm(imgB_T))
+    skl = 1 - cosine_distances(imageA,imageB)
+    # s = ssim(imageA, imageB)
     return skl
 
 @views.route('/delete-image', methods=['POST'])
@@ -229,6 +352,7 @@ def delete_image():
     image = Image.query.get(imgId)
     if image:
         if image.user_id == current_user.id:
+            os.remove(image.img)
             db.session.delete(image)
             db.session.commit()
     return jsonify({})
